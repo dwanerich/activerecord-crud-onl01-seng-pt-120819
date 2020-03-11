692c5e285a2aa582c3c79973fcc1765313e6b164
dwane richards <dwanerich@gmail.com>
2020-03-11 00:04:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block(title: "The Room")
+def can_be_created_in_a_block(movie)
   Movie.new do |m|
   m.title = "Home Alone"
   m.release_date = 1990
