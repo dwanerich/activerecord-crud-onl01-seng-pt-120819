f3ab0da250084f73d05c747827a890ce38578b40
dwane richards <dwanerich@gmail.com>
2020-03-11 00:52:38 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block(hash)
+def can_be_created_in_a_block(hash = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
   m.title = hash[:title]
   m.release_date = hash[:release_date]
